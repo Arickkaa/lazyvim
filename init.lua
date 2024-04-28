580ec6d01e812141d3577988fea3e9e848156f4c
Aric <aric@MacBook-Pro.local>
2024-04-28 14:35:20 +0800

    stable v1.1.1 configure neovide

--- init.lua
+++ init.lua
@@ -1,2 +1,12 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+--透明度
+vim.g.neovide_transparency = 0.95
+
+-- cursor特效
+vim.g.neovide_cursor_vfx_mode = "railgun"
+--vim.g.neovide_cursor_vfx_mode = "ripple"
+
+vim.g.neovide_window_blurred = true
+vim.o.guifont = "JetBrainsMono Nerd Font:h16"
